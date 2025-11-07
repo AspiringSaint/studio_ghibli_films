a2f4416a9720960e56a7f7bae8b2541387fd1593
AspiringSaint <wilberttosoydev@outlook.com>
2025-11-07 11:43:51 +0800

    added the filmcard initial setup

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -1,41 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import type { Film } from '../types/Film';
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+const props = defineProps<{ film: Film }>();
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+    <div class="image-wrapper">
+      <img :src="film.image" :alt="film.title">
+    </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <div class="details-wrapper">
+      <div class="text-content">
+        <h2>{{ film.title }}</h2>
+        <span class="original_title">{{ film.original_title }}</span>
+        <p>{{ film.description }}</p>
+      </div>
+
+      <div class="meta">
+        <div class="meta-item">
+          <span>{{ film.release_date }}</span>
+        </div>
+
+        <div class="meta-item">
+          <span>{{ film.rt_score }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
