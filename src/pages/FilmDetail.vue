9da76ffe8fe54122104a435c986c821876e33481
AspiringSaint <wilberttosoydev@outlook.com>
2025-11-07 11:16:15 +0800

    added the necessary components and pages

---
+++ src/pages/FilmDetail.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>Film Detail</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/FilmList.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>Film List</div>
+</template>
+
+<style scoped>
+
+</style>
