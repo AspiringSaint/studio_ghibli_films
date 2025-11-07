4a994e658908de947d5e7b7a671cd3d3e650e9f9
AspiringSaint <wilberttosoydev@outlook.com>
2025-11-07 11:13:36 +0800

    added the film type

---
+++ src/types/Film.ts
@@ -0,0 +1,13 @@
+export interface Film {
+    id: string;
+    title: string;
+    original_title: string;
+    description: string;
+    release_date: string;
+    rt_score: string;
+    image: string;
+    movie_banner: string;
+    director: string;
+    producer: string;
+    running_time: string;
+}
